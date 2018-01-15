f798530cc2b54b4eba525d57dd3192bb12bc52c2
Victor Bu <victorbu.xx@gmail.com>
2018-01-15 22:56:36 +0800

    Topshelf

---
+++ Topshelf.Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Topshelf.Demo
+{
+    /// <summary>
+    /// 
+    /// Install-Package Topshelf -Version 3.3.1
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<ServiceRunner>();
+
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Topshelf Service Demo");
+                x.SetDisplayName("Topshelf Service");
+                x.SetServiceName("TopshelfService");
+
+                x.EnablePauseAndContinue();
+            });
+        }
+    }
+}
---
+++ Topshelf.Demo/ServiceRunner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Timers;
+
+namespace Topshelf.Demo
+{
+    public class ServiceRunner : ServiceControl
+    {
+        private readonly Timer timer;
+
+        public ServiceRunner()
+        {
+            timer = new Timer(1000) { AutoReset = true };
+
+            timer.Elapsed += new ElapsedEventHandler(Execute);
+        }
+        public bool Start(HostControl hostControl)
+        {
+            timer.Start();
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            timer.Stop();
+
+            return true;
+        }
+
+        private static void Execute(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
