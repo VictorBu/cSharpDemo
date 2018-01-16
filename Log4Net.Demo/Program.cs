5f784bb4ec8ad8749909e6dbb88179b4f71f2fb4
Victor Bu <victorbu.xx@gmail.com>
2018-01-16 21:36:14 +0800

    log4net

---
+++ Log4Net.Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using log4net;
+
+namespace Log4Net.Demo
+{
+    /// <summary>
+    /// 
+    /// Install-Package log4net -Version 1.2.10
+    /// 
+    /// log4net.config 要设置为 Copy always
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo("log4net.config"));
+
+            ILog logger = LogManager.GetLogger("RollingLogFileAppender");
+
+            logger.Info("log information");
+
+            logger.Warn("log warning");
+
+            logger.Error("log error", new Exception("exception information"));
+
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 > 为方便调试解决方案右键 -> Properties -> Startup Project -> Current selection
 
 1. Quartz
-
+1. Topshelf
 
